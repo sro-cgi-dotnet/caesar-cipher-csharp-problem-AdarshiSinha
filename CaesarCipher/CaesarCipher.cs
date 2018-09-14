b91d37499b9882baa1d8a41535e830552c089f6c
Adarshi Sinha <adarshisinha@gmail.com>
2018-09-14 16:30:32 +0530

    Ass6

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,29 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string s=null;
+            int n=shiftKey%26;
+            foreach(char c in text)
+            {
+                if((c>='a'&& c<='z' ))
+                  {
+                      if(((int)c+n)>122)
+                        s+=(char)(97+(c+n-123));
+                     else
+                        s+=(char)(c+n);
+                  }
+                else if(c>='A' && c<='Z')
+                {
+                    if(((int)c+n)>90)
+                        s+=(char)(65 +(c+n-91));
+                    else
+                        s+=(char)(c+n);
+                }
+                else
+                   s+=c;
+            }
+            return s;
+           // throw new NotImplementedException("You need to implement this function.");
         }
     }
 }
